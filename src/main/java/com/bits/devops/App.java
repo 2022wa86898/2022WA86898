b30578430dca69428124b0b8a1cdbcbb387b2737
Himanshu Pathak <2022wa86898@bits-pilani.ac.in>
2026-09-20 21:28:28 +0530

    Modified source code for continuous integration

--- src/main/java/com/bits/devops/App.java
+++ src/main/java/com/bits/devops/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Continuous Integration completed successfully by 2022wa86898");
     }
 }
